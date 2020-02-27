3cee317460a6c141f6677bcdfb5abd01d13b4a9c
brad-bartlett <bbartlett222@gmail.com>
2020-02-27 12:11:50 +1100

    search and calculate comps

---
+++ app/javascript/components/Calculate.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Calculate extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return(
+            <div>Calculate</div>
+        )
+    }
+}
+
+export default Calculate
