e89ed38314a4b8e93e20786bb58a54eaf615f6a2
brad-bartlett <bbartlett222@gmail.com>
2020-02-27 12:39:43 +1100

    axios added

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,10 @@
 import React, {Component} from 'react'
 import PortfolioContainer from './PortfolioContainer'
+import axios from 'axios'
+
+const csrfToken = document.querySelector('[name="csrf-token"]').content
+axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+// sets token in headers before post request is made 
 
 class App extends Component {
     render() {
--- app/javascript/components/PortfolioContainer.js
+++ app/javascript/components/PortfolioContainer.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Search from './Search'
 import Calculate from './Calculate'
+import axios from 'axios'
 
 class PortfolioContainer extends Component {
     constructor(props) {
@@ -17,8 +18,17 @@ class PortfolioContainer extends Component {
     }
 
     handleChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value
+        // this.setState({
+        //     [e.target.name]: e.target.value
+        // })
+        axios.post('http://localhost:3000/search',{
+            search: e.target.value
+        })
+        .then((data) => {
+            debugger
+        })
+        .catch((data) => {
+            debugger
         })
         console.log(this.state.name)
     }
