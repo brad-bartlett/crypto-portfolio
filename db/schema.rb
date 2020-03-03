903b8a4bde8b2c2044abec8619fcb632fca35bd3
brad-bartlett <bbartlett222@gmail.com>
2020-03-03 21:03:47 +1100

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_02_26_235624) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "currencies", force: :cascade do |t|
     t.string "name"
     t.string "description"
